88031b258503d88202eaf11d7be2960abf1c5bda
Jesus Recio <jrecio@izertis.com>
2025-09-01 14:26:47 +0200

    feat: add a waiter for appium to be ready

--- runAppium.sh
+++ runAppium.sh
@@ -4,3 +4,13 @@ npm install -g appium
 appium -v
 appium driver install uiautomator2
 appium --allow-insecure=adb_shell &>/dev/null &
+echo "Initializing Appium..."
+while true; do
+    if curl -s http://localhost:4723/status | jq -e '.value.ready' >/dev/null 2>&1; then
+        echo "Appium is ready!"
+        break
+    else
+        echo "Waiting for Appium to be ready..."
+        sleep 1
+    fi
+done
