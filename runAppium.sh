cb24ecb60d1bbf89d26a5ce062d032c5544eec41
Jesus Recio <jrecio@izertis.com>
2025-09-01 15:15:53 +0200

    feat: redirect Appium output to appium.log file

--- runAppium.sh
+++ runAppium.sh
@@ -3,7 +3,7 @@ set -ex
 npm install -g appium@2.19.0
 appium -v
 appium driver install uiautomator2@4.2.9
-appium --allow-insecure=adb_shell &>/dev/null &
+appium --allow-insecure=adb_shell > appium.log 2>&1 &
 echo "Initializing Appium..."
 while true; do
     if curl -s http://localhost:4723/status | jq -e '.value.ready' >/dev/null 2>&1; then
