f3fc7c6611e93d3c5faaeef6c1e10bd85e56009d
Jesus Recio <jrecio@izertis.com>
2025-09-10 18:22:43 +0200

    feat: update dependencies to latest. Adapt to Appium3

--- runAppium.sh
+++ runAppium.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -ex
-npm install -g appium@2.19.0
+npm install -g appium@latest
 appium -v
-appium driver install uiautomator2@4.2.9
-appium --allow-insecure=adb_shell > appium.log 2>&1 &
+appium driver install uiautomator2@latest
+appium --allow-insecure='*:adb_shell' > appium.log 2>&1 &
 echo "Initializing Appium..."
 while true; do
     if curl -s http://localhost:4723/status | jq -e '.value.ready' >/dev/null 2>&1; then
