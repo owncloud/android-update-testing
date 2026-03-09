c66e258d2cf00c62a20aabf10ca643ceaf2a8bf2
Jesus Recio <jrecio@izertis.com>
2026-03-09 08:45:52 +0100

    fix: keep penultimate appium version till new one stuff is fixed

--- runAppium.sh
+++ runAppium.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -ex
-npm install -g appium@latest
+npm install -g appium@3.1.0
 appium -v
 appium driver install uiautomator2@latest
 appium --allow-insecure='*:adb_shell' > appium.log 2>&1 &
